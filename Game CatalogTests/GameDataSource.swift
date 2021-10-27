7dfcb8622694b20563b18c7df2bc65e08f96743f
jan <jan@code.id>
2021-10-27 18:59:37 +0700

    implement UT GameDataSource

--- Game CatalogTests/GameDataSource.swift
+++ Game CatalogTests/GameDataSource.swift
@@ -6,27 +6,60 @@
 //
 
 import XCTest
+@testable import Game_Catalog
+
+var gameDataSource: GameDataSourceProtocol?
 
 class GameDataSource: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        gameDataSource = GameDataSourceImp()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
+        gameDataSource = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testGetGameList() {
+        
+        let promise = expectation(description: "Response Success")
+        
+        gameDataSource?.getListGame(page: 1, result: { listGame in
+            if listGame.count != 0 && listGame.count == 10 {
+                promise.fulfill()
+            } else {
+                XCTFail("not fullfil the condition")
+            }
+        }, error: {
+            XCTFail("error get list of games")
+        })
+        wait(for: [promise], timeout: 5)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testGetGameDetail() {
+        
+        let promise = expectation(description: "Response Success")
+        
+        gameDataSource?.getDetailGame(id: 3498, result: { gameDetail in
+            if gameDetail.id == 3498 {
+                promise.fulfill()
+            } else {
+                XCTFail("data not same")
+            }
+        }, error: {
+            XCTFail("error get detail of game")
+        })
+        wait(for: [promise], timeout: 5)
     }
+    
+//    func testFailedGetGameList() {
+//        
+//    }
+//    
+//    func testFailedGetGameDetail() {
+//        
+//    }
 
 }
